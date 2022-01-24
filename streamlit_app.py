7ca5820160170a809cb64c692faee433317faa13
Robert Ritz <robertritz@outlook.com>
2022-01-24 14:43:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,13 @@ import gdown
 
 st.markdown("""# Mongolian Five Animal Classifier
 
-Mongolia has five traditional herd animals: Horses, Cattle, Sheep, Goats, and Camels. This app can perform image classification on these animals. Upload an image and try it out!""")
+Mongolia has five traditional herd animals: Horses, Cattle, Sheep, Goats, and Camels. This app allows you to upload an image of one of these five animals and the connected model will classify it for you. Upload an image and try it out!
+
+This app was created as a demo for the Deep Learning course at LETU Mongolia American University.""")
 
 st.markdown("""### Upload your image here""")
 
-image_file = st.file_uploader("Upload an Image", type=["png","jpg","jpeg"])
+image_file = st.file_uploader("Image Uploader", type=["png","jpg","jpeg"])
 
 ## Model Loading Section
 model_path = Path("export.pkl")
@@ -22,11 +24,13 @@ if not model_path.exists():
 else:
     learn_inf = load_learner('export.pkl')
 
+col1, col2 = st.columns(2)
 if image_file is not None:
     img = PILImage.create(image_file)
-    st.image(img)
-
     pred, pred_idx, probs = learn_inf.predict(img)
 
-    st.markdown(f"""**This is an image of:** {pred}""")
-    st.markdown(f"""Confidence: {max(probs.tolist())}""")
+    with col1:
+        st.markdown(f"""### Predicted animal: {pred.capitalize()}""")
+        st.markdown(f"""### Probability: {round(max(probs.tolist()), 3) * 100}%""")
+    with col2:
+        st.image(img, width=300)
